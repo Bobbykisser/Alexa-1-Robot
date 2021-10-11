0a24533e78d484c76d6e9cd6825c8b8b5dfbdae9
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-10-11 12:22:45 +0530

    Update ping.py

--- MashaRoBot/modules/ping.py
+++ MashaRoBot/modules/ping.py
@@ -74,7 +74,7 @@ def ping(update: Update, context: CallbackContext):
     msg = update.effective_message
 
     start_time = time.time()
-    message = msg.reply_text("Pinging...")
+    message = msg.reply_text("𝐏𝐢𝐧𝐠𝐢𝐧𝐠...")
     end_time = time.time()
     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
