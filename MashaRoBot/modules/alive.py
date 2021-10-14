a97a8ac68208f34e6209b033d3900ca9e087882e
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-10-14 17:21:05 +0530

    Update alive.py

--- MashaRoBot/modules/alive.py
+++ MashaRoBot/modules/alive.py
@@ -8,9 +8,9 @@ PHOTO = "https://telegra.ph/file/1827ec3f0fd066a7c2874.jpg"
 async def awake(event):
   WOLFX = event.sender.first_name
   WOLFX = "**🐺 𝐈 𝐀𝐦, 𝐖𝐨𝐥𝐟𝐗 ⚔️** \n\n"
-  WOLFX += "**🐺 I'm Working with awesome speed**\n\n"
+  WOLFX += "**🐺 𝐖𝐨𝐥𝐟𝐗 𝐈𝐬 𝐏𝐞𝐫𝐟𝐞𝐜𝐭𝐥𝐲 𝐇𝐨𝐰𝐥𝐢𝐧𝐠**\n\n"
   WOLFX += "**🐺 𝐖𝐨𝐥𝐟𝐗 : 2.0 𝐋𝐀𝐓𝐄𝐒𝐓**\n\n"
   WOLFX += "**🐺 𝐌𝐲 𝐎𝐰𝐧𝐞𝐫 :** [Hᴀᴄᴋᴇʀ 888+](https://t.me/joinchat/r9qx47U5xEZjY2E1)\n\n"
-  WOLFX += "**🐺 Telethon Version : 1.23.0**\n\n"
-  BUTTON = [[Button.url("𝐒𝐔𝐏𝐏𝐎𝐑𝐓🙂", "https://t.me/LOVELYAPPEAL"), Button.url("𝐔𝐏𝐃𝐀𝐓𝐄", "https://t.me/LOVELY_ROBOTS")]]
+  WOLFX += "**🐺 𝐓𝐞𝐥𝐞𝐭𝐡𝐨𝐧 𝐕𝐞𝐫𝐬𝐢𝐨𝐧 : 1.23.0**\n\n"
+  BUTTON = [[Button.url("𝐒𝐔𝐏𝐏𝐎𝐑𝐓🗡️", "https://t.me/wolfxbotz"), Button.url("𝐔𝐏𝐃𝐀𝐓𝐄👨‍💻", "https://t.me/joinchat/r9qx47U5xEZjY2E1")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=WOLFX,  buttons=BUTTON)
