4e950b50e734b7dd3cf3005768ba577e9bf8cf48
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2021-10-15 08:31:07 +0530

    Update alive.py

--- MashaRoBot/modules/alive.py
+++ MashaRoBot/modules/alive.py
@@ -10,7 +10,7 @@ async def awake(event):
   WOLFX = "**🐺 𝐈 𝐀𝐦, 𝐖𝐨𝐥𝐟𝐗 ⚔️** \n\n"
   WOLFX += "**🐺 𝐖𝐨𝐥𝐟𝐗 𝐈𝐬 𝐏𝐞𝐫𝐟𝐞𝐜𝐭𝐥𝐲 𝐇𝐨𝐰𝐥𝐢𝐧𝐠**\n\n"
   WOLFX += "**🐺 𝐖𝐨𝐥𝐟𝐗 : 2.0 𝐋𝐀𝐓𝐄𝐒𝐓**\n\n"
-  WOLFX += "**🐺 𝐌𝐲 𝐎𝐰𝐧𝐞𝐫 :** [Hᴀᴄᴋᴇʀ 888+](https://t.me/joinchat/r9qx47U5xEZjY2E1)\n\n"
+  WOLFX += "**🐺 𝐌𝐲 𝐎𝐰𝐧𝐞𝐫 :** [Hᴀᴄᴋᴇʀ 888+](https://t.me/HMF_OWNER_1)\n\n"
   WOLFX += "**🐺 𝐓𝐞𝐥𝐞𝐭𝐡𝐨𝐧 𝐕𝐞𝐫𝐬𝐢𝐨𝐧 : 1.23.0**\n\n"
   BUTTON = [[Button.url("𝐒𝐔𝐏𝐏𝐎𝐑𝐓🗡️", "https://t.me/wolfxbotz"), Button.url("𝐔𝐏𝐃𝐀𝐓𝐄👨‍💻", "https://t.me/joinchat/r9qx47U5xEZjY2E1")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=WOLFX,  buttons=BUTTON)
